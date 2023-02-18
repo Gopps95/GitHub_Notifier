94e00127000a08d5ab3e56913409e574282565c6
gopps95 <shankargopi7@gmail.com>
2023-02-18 21:54:29 +0530

    background_done

--- background.js
+++ background.js
@@ -1,4 +1,19 @@
-while(1!=0){
-    console.log("hey")
 
-}
+chrome.browserAction.onClicked.addListener(function(tab) {
+    
+    chrome.runtime.openOptionsPage();
+  });
+  
+
+  chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if (message.type === "notification") {
+      
+      chrome.notifications.create({
+        type: "basic",
+        iconUrl: "icon.png",
+        title: "GitHub Notification",
+        message: message.text
+      });
+    }
+  });
+  
