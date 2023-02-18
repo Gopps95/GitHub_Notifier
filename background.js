39b5c62b61af985d796df80d3d5321a0e19141d5
gopps95 <shankargopi7@gmail.com>
2023-02-19 00:41:34 +0530

    update

--- background.js
+++ background.js
@@ -18,14 +18,14 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   }); 
 const { Octokit } = require("@octokit/rest");
 
-const octokit = new Octokit({ auth: "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" });
-
-octokit.pulls.create({
-  owner: "my-org",
-  repo: "my-repo",
-  title: "My pull request",
-  head: "my-feature-branch",
-  base: "main",
-}).then((response) => {
-  console.log(response.data);
+  const octokit = new Octokit({ auth: "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" });
+  octokit.pulls.create({
+    owner: "my-org",
+    repo: "my-repo",
+    title: "My pull request",
+    head: "my-feature-branch",
+    base: "main",
+  }).then((response) => {
+    console.log(response.data);
+  
 });
