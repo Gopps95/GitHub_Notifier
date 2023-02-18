872ae46ef0f2df8da03973a1ef98899b1fc321ab
gopps95 <shankargopi7@gmail.com>
2023-02-18 22:54:02 +0530

    new_commit

--- background.js
+++ background.js
@@ -16,4 +16,15 @@ chrome.browserAction.onClicked.addListener(function(tab) {
       });
     }
   });
-  
+  const express = require('express')
+const app = express()
+
+app.use(manifest.json())
+
+app.post('/webhook', (req, res) => {
+  const data = req.body
+  // Do something with the data
+  res.send('Webhook received')
+})
+
+app.listen(3000, () => console.log('Webhook server listening on port 3000'))
